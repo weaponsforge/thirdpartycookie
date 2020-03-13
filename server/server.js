e5796370fd1e6722ee2b745333af2cefcd31b5d6
weaponsforge <forgeweapons@gmail.com>
2020-03-14 04:42:00 +0800

    WIP: Used express to set/clear cookies.

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const PORT = process.env.PORT || 3001
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const app = express()
+const cookieName = 'thirdparty'
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.get('/check', (req, res) => {
+  res.cookie(cookieName, 'yes', {
+    maxAge: 360000,
+    sameSite: 'none',
+    secure: true
+  }).redirect('process')
+})
+
+app.get('/process', (req, res, next) => {
+  const isEnabled = (req.cookies[cookieName] !== undefined)
+  res.clearCookie(cookieName)
+
+  if (isEnabled) {
+    res.status(200).send({
+      message: 'OK'
+    })
+  } else {
+    next(new Error('Cookies are not set'))
+  }
+})
+
+app.get('/hello', () => {
+  res.status(200).send({
+    message: 'hello'
+  })
+})
+
+app.use((error, req, res, next) => {
+  res.status(500).send({
+    message: error.message
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`third party server running on http://localhost:${PORT}`)
+})
